0d2ce69d4c43074afc7b76e311454347e40ed1b8
hanyeol1425 <hangyeolg0205@gmail.com>
2024-08-22 23:24:21 +0900

    DB 설정값 env으로 고침

--- backend/db.js
+++ backend/db.js
@@ -1,8 +1,16 @@
+require('dotenv').config();
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('mydb', 'root', 'Fjqm1425*', {
-    host: 'localhost',
-    dialect: 'mysql',
+const database = process.env.DATABASE
+const host = process.env.DATABASE_HOST
+const user = process.env.DATABASE_USER
+const password = process.env.DATABASE_PASSWORD
+const dialect = process.env.DATABASE_DIALECT
+const port = process.env.DATABASE_PORT
+
+const sequelize = new Sequelize(database, user, password, {
+    host: host,
+    dialect: dialect,
     port: 3306,
 });
 
